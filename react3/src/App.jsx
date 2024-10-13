158eb8ef74ad537a1702b2bda30d1b46c2f70e8f
DevBilalAhmad <bsem-s20-021@superior.edu.pk>
2024-10-13 14:57:12 -0700

    Fix line ending warnings

---
+++ react3/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      React 3
+    </div>
+  )
+}
+
+export default App
---
+++ react4/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      React 4
+    </div>
+  )
+}
+
+export default App
